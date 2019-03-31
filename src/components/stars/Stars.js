728dc5d9d77541b3d164966621c6eecdcec31a28
amku91 <amit.kumar@wellthy.care>
2019-03-31 13:14:27 +0530

    [ADD] Stars Tab Component

--- src/components/stars/Stars.js
+++ src/components/stars/Stars.js
@@ -2,51 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
 import { Placeholder, Container, Grid, Segment, Icon, Divider, Input, Dropdown, Button } from 'semantic-ui-react';
-import './Projects.css';
+import './Stars.css';
 
-const Projects = ({ username, reposData }) => {
+const Stars = ({ reposData }) => {
     return (
         <div>
-            <Segment placeholder>
-    <Header icon>
-      <Icon name='search' />
-      You don't have any projects yet.
-    </Header>
-    <Segment.Inline>
-      <Button primary>Learn More</Button>
-      <Button>New Project</Button>
-    </Segment.Inline>
-  </Segment>
             <Container>
                 <Grid columns={16}>
                     <Grid.Row>
-                        <Grid.Column width={8}>
-                            <Input fluid icon='search' placeholder='Find a repository...' />
+                        <Grid.Column width={10}>
+                            <div className='stars-repo-title'>
+                                REPOSITORIES
+                            </div>
                         </Grid.Column>
-                        <Grid.Column width={3}>
-                            <Dropdown text='Filter Tags' floating labeled button icon='filter' fluid className='icon'>
-                                <Dropdown.Menu>
+                        <Grid.Column width={6}>
+                            <Dropdown text='Filter' icon='filter' floating labeled button className='icon' direction='right'>
+                                <Dropdown.Menu position='right'>
+                                    <Dropdown.Header content='Search Issues' />
+                                    <Input icon='search' iconPosition='left' name='search' />
                                     <Dropdown.Header icon='tags' content='Filter by tag' />
                                     <Dropdown.Divider />
-                                    <Dropdown.Item description='2 new' text='Important' />
-                                    <Dropdown.Item description='10 new' text='Hopper' />
-                                    <Dropdown.Item description='5 new' text='Discussion' />
+                                    <Dropdown.Item label={{ color: 'red', empty: true, circular: true }} text='Important' />
+                                    <Dropdown.Item label={{ color: 'blue', empty: true, circular: true }} text='Announcement' />
+                                    <Dropdown.Item label={{ color: 'black', empty: true, circular: true }} text='Discussion' />
                                 </Dropdown.Menu>
                             </Dropdown>
-                        </Grid.Column>
-                        <Grid.Column width={3}>
-                            <Dropdown text='Filter Tags' floating labeled button icon='filter' fluid className='icon'>
-                                <Dropdown.Menu>
-                                    <Dropdown.Header icon='tags' content='Filter by tag' />
-                                    <Dropdown.Divider />
-                                    <Dropdown.Item description='2 new' text='Important' />
-                                    <Dropdown.Item description='10 new' text='Hopper' />
-                                    <Dropdown.Item description='5 new' text='Discussion' />
-                                </Dropdown.Menu>
-                            </Dropdown>
-                        </Grid.Column>
-                        <Grid.Column width={2}>
-                            <Button primary>New</Button>
                         </Grid.Column>
                     </Grid.Row>
                 </Grid>
@@ -96,9 +76,8 @@ const Projects = ({ username, reposData }) => {
     );
 };
 
-Projects.propTypes = {
-    username: PropTypes.string.isRequired,
+Stars.propTypes = {
     reposData: PropTypes.arrayOf(Object).isRequired
 };
 
-export default Projects;
+export default Stars;
